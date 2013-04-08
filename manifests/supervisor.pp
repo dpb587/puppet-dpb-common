66bdc99fe6d441814dc08f124636019c30e86be6
Danny Berger <dpb587@gmail.com>
2013-04-08 15:01:43 -0600

    supervisor workarounds

---
+++ manifests/supervisor.pp
@@ -0,0 +1,33 @@
+class dpb-common::supervisor {
+    exec {
+        '/usr/bin/easy_install' :
+            command => '/usr/bin/wget http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg && /bin/sh setuptools-0.6c11-py2.6.egg && /bin/rm setuptools-0.6c11-py2.6.egg',
+            creates => '/usr/bin/easy_install',
+            cwd => '/tmp/',
+            ;
+        '/usr/bin/supervisord' :
+            command => '/usr/bin/easy_install supervisor',
+            creates => '/usr/bin/supervisord',
+            require => [
+                Exec['/usr/bin/easy_install'],
+            ],
+            ;
+    }
+
+    file {
+        '/etc/init.d/supervisord' :
+            source => 'puppet:///dpb-common/files/initd-supervisord',
+            owner => 'root',
+            group => 'root',
+            mode => 0755,
+            ;
+    }
+
+    package {
+        'supervisor' :
+            noop => true,
+            ;
+    }
+
+    include supervisor
+}
