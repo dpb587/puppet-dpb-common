b3ab950dca0e1bbcf0f050b7add1733e77861eac
Danny Berger <dpb587@gmail.com>
2013-04-11 10:22:37 -0600

    disable iptables

--- manifests/srv/vagrant.pp
+++ manifests/srv/vagrant.pp
@@ -1,4 +1,10 @@
 class dpb-common::srv::vagrant {
+    service {
+        'iptables' :
+            ensure => stopped,
+            ;
+    }
+
     package {
         [ 'ant', 'man', 'ntp', 'telnet', 'wget', 'vim-enhanced' ] :
             ensure => installed,
